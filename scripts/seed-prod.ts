05c6e8e905f3fa8cf5585616f1e7899f236ebd05
Sleyter2616 <62727289+Sleyter2616@users.noreply.github.com>
2024-12-20 13:00:13 -0500

    fix: simplify database connection for Vercel deployment

--- scripts/seed-prod.ts
+++ scripts/seed-prod.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import * as bcrypt from 'bcryptjs'
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.POSTGRES_PRISMA_URL,
-    },
-  },
-})
+const prisma = new PrismaClient()
 
 async function main() {
   // Delete all existing data
